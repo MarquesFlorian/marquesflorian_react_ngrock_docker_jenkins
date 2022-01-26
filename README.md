b1498a73ddebbd270c945ae2680cb1097305f432
Florian Marques <florian.marques@efrei.net>
2022-01-26 18:45:44 +0100

    Add docker configuration, working well

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+## Use the project
+- Launch Docker
+- Use `docker compose up` to build and deploy the docker
+- If at the first `docker compose up` they're is a problem, relaunch the command (and do not delete the previous build and images), it will work.
+- Go on "http://localhost:8080", at each loading, it will increment a value into the database, the number of all incrementations is printed
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './florian.jpg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Khodor
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/florian-marques-b84b4515b/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          TP of Florian MARQUES
         </a>
       </header>
     </div>
